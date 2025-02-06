d35eb994b3cf134113987cb60e1a8101be993cbe
chacha86 <taejincha0619@gmail.com>
2025-02-06 09:49:54 +0900

    listed g

--- src/main/java/com/example/rest_tdd/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/example/rest_tdd/domain/post/post/controller/ApiV1PostController.java
@@ -2,6 +2,7 @@ package com.example.rest_tdd.domain.post.post.controller;
 
 import com.example.rest_tdd.domain.member.member.entity.Member;
 import com.example.rest_tdd.domain.post.post.dto.PostDto;
+import com.example.rest_tdd.domain.post.post.dto.PostWithContentDto;
 import com.example.rest_tdd.domain.post.post.entity.Post;
 import com.example.rest_tdd.domain.post.post.service.PostService;
 import com.example.rest_tdd.global.Rq;
@@ -12,6 +13,8 @@ import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/v1/posts")
 @RequiredArgsConstructor
@@ -20,6 +23,21 @@ public class ApiV1PostController {
     private final PostService postService;
     private final Rq rq;
 
+    @GetMapping
+    public RsData<List<PostDto>> getItems() {
+
+        List<Post> posts = postService.getListedItems();
+
+        return new RsData<>(
+                "200-1",
+                "글 목록 조회가 완료되었습니다.",
+                posts.stream()
+                        .map(PostDto::new)
+                        .toList()
+        );
+
+    }
+
     @GetMapping("{id}")
     public RsData<PostDto> getItem(@PathVariable long id) {
 
@@ -27,7 +45,7 @@ public class ApiV1PostController {
                 () -> new ServiceException("404-1", "존재하지 않는 글입니다.")
         );
 
-        if(!post.isPublished()) {
+        if (!post.isPublished()) {
             Member actor = rq.getAuthenticatedActor(); //
             post.canRead(actor);
         }
@@ -58,7 +76,8 @@ public class ApiV1PostController {
         );
     }
 
-    record ModifyReqBody(@NotBlank String title, @NotBlank String content) {}
+    record ModifyReqBody(@NotBlank String title, @NotBlank String content) {
+    }
 
     @PutMapping("{id}")
     public RsData<PostDto> modify(@PathVariable long id, @RequestBody @Valid ModifyReqBody reqBody) {
